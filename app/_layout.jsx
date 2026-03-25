3f06d74e7b9b960b0d6491773b9006534248531b
andrew0samir <andrewsamir317@gmail.com>
2026-03-25 21:06:57 +0200

    adding tabs navigation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -18,6 +18,7 @@ const RootLayout = () => {
         }}
       >
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
 
         <Stack.Screen
           name="index"
